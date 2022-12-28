484f6cec43577c5931d2290720c4391bc08e7adb
Joseph Youngjae Lee <116613246+leejaejoseph@users.noreply.github.com>
2022-12-28 00:37:50 +0000

    At the bottom of data.js, include a conditional statement that:, checks to see if there is a JSON data model stored in localStorage., if there is, parse it and assign it to the initial data variable so that the user can continue adding entries to the pre-existing data model.

--- js/data.js
+++ js/data.js
@@ -12,3 +12,7 @@ window.addEventListener('beforeunload', JSONStorage);
 function JSONStorage(event) {
   localStorage.setItem('data-in-JSON', JSON.stringify(data));
 }
+
+if (localStorage.getItem('data-in-JSON') !== null) {
+  data = JSON.parse(localStorage.getItem('data-in-JSON'));
+}
