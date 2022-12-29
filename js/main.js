4ab303359bd89bb39f0c2941b4d79a8adbeffef6
Joseph Youngjae Lee <116613246+leejaejoseph@users.noreply.github.com>
2022-12-29 18:03:43 +0000

    Revised Issue-2 for pull-request. #2

--- js/main.js
+++ js/main.js
@@ -21,60 +21,52 @@ function submission(event) {
   data.entries.unshift(formObject);
   $newImage.src = 'images/placeholder-image-square.jpg';
   $form.reset();
-  getEntry(data.entries[0]);
+  renderEntry(data.entries[0]);
   toggleNoEntries();
   viewEntries();
 }
 
 function renderEntry(entry) {
-  var $entriesImg = document.getElementById('img' + entry.entryId);
-  var $entriesH2 = document.getElementById('h2' + entry.entryId);
-  var $entriesP = document.getElementById('p' + entry.entryId);
-  $entriesImg.src = entry.imageUrl;
-  $entriesH2.textContent = entry.title;
-  $entriesP.textContent = entry.notes;
+  var $ul = document.querySelector('ul');
+  var $li = document.createElement('li');
+  $li.setAttribute('class', 'row');
+  $ul.appendChild($li);
+
+  var $divImg = document.createElement('div');
+  $divImg.setAttribute('class', 'column-half');
+  $li.appendChild($divImg);
+
+  var $img = document.createElement('img');
+  $img.src = entry.imageUrl;
+  var imgEntry = 'img' + entry.entryId;
+  $img.setAttribute('id', imgEntry);
+  $divImg.appendChild($img);
+
+  var $divText = document.createElement('div');
+  $divText.setAttribute('class', 'column-half');
+  $li.appendChild($divText);
+
+  var $h2 = document.createElement('h2');
+  $h2.textContent = entry.title;
+  var h2Entry = 'h2' + entry.entryId;
+  $h2.setAttribute('id', h2Entry);
+  $divText.appendChild($h2);
+
+  var $p = document.createElement('p');
+  $p.textContent = entry.notes;
+  var pEntry = 'p' + entry.entryId;
+  $p.setAttribute('id', pEntry);
+  $divText.appendChild($p);
+  return $ul;
 }
 
-document.addEventListener('DOMContentLoaded', getEntry('contentLoad'));
+document.addEventListener('DOMContentLoaded', getEntry);
 
 function getEntry(entry) {
-  var length = 0;
-  if (entry === 'contentLoad') {
-    length = data.entries.length;
-    toggleNoEntries();
-  } else {
-    length = 1;
-  }
-  for (var i = 0; i < length; i++) {
-    var $ul = document.querySelector('ul');
-    var $li = document.createElement('li');
-    $li.setAttribute('class', 'row');
-    $ul.appendChild($li);
-
-    var $divImg = document.createElement('div');
-    $divImg.setAttribute('class', 'column-half');
-    $li.appendChild($divImg);
-
-    var $img = document.createElement('img');
-    var imgEntry = 'img' + data.entries[i].entryId;
-    $img.setAttribute('id', imgEntry);
-    $divImg.appendChild($img);
-
-    var $divText = document.createElement('div');
-    $divText.setAttribute('class', 'column-half');
-    $li.appendChild($divText);
-
-    var $h2 = document.createElement('h2');
-    var h2Entry = 'h2' + data.entries[i].entryId;
-    $h2.setAttribute('id', h2Entry);
-    $divText.appendChild($h2);
-
-    var $p = document.createElement('p');
-    var pEntry = 'p' + data.entries[i].entryId;
-    $p.setAttribute('id', pEntry);
-    $divText.appendChild($p);
-
+  for (var i = 0; i < data.entries.length; i++) {
     renderEntry(data.entries[i]);
+    toggleNoEntries();
+    viewEntries();
   }
 }
 
